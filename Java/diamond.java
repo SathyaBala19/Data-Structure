baf97c966b132f71011ff588673b681036d3f05b
SathyaBala19 <sathyabala1934@gmail.com>
2026-07-21 14:51:00 +0530

    commit

---
+++ Java/diamond.java
@@ -0,0 +1,44 @@
+package Java;
+
+import java.util.Scanner;
+
+public class diamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
