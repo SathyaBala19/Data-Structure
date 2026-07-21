c0f61f0102e2c79f43ed9061d478a493d62357dc
SathyaBala19 <sathyabala1934@gmail.com>
2026-07-21 12:10:33 +0530

    commit

---
+++ Java/specialNumber.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class specialNumber {
+    public static void main(String[] args) {
+        int s = 10;
+        while (s <= 45) {
+            int a = s % 10;
+            int b = s / 10;
+
+            int multi = a * b;
+            int sum = a + b;
+
+            if (s == (multi + sum)) {
+                System.out.println(s);
+            }
+            
+            s += 1;
+        }
+    }
+    
+}
