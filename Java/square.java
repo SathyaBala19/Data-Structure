f470c08ed2390846751cbb33bc19a11de75dc57c
SathyaBala19 <sathyabala1934@gmail.com>
2026-07-21 15:13:32 +0530

    commit

--- Java/square.java
+++ Java/square.java
@@ -1,19 +1,23 @@
 package Java;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class square {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for (int i = 1; i<=n; i++) {
-            for (int j = 1; j<=i; j++) {
-                System.out.print(j + " ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
+
         sc.close();
     }
-    
 }
