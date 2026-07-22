4d1f7bf443e0fde3bbd5db79cd25ae62312264b8
SathyaBala19 <sathyabala1934@gmail.com>
2026-07-22 11:24:07 +0530

    commit

---
+++ Java/college problems/pattern.java
@@ -0,0 +1,27 @@
+public class pattern {
+    public static void patternn(int n) {
+        int y = n-1;
+        while (y >= 0) {
+            int i = 0;
+            while (i < y) {
+                System.out.print(" ");
+                i++;
+            }
+            int k = 0;
+            while (k + y < n) {
+                if ((k%y) != 0) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("* ");
+                }
+                k++;
+            }
+            System.out.println();
+            y--;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
